b84a555c935e00b97fa0e9e6137a2daa0b128c3f
Dawid Wiącek <dawid.wiacek@rst.com.pl>
2017-07-30 09:55:41 +0200

    [ADD] Connecting ViewModel with view, creating in view textviews displaying weahter conditions

--- app/src/main/java/com/wiacek/weatherapp/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/wiacek/weatherapp/ui/weather/WeatherActivity.kt
@@ -1,8 +1,10 @@
 package com.wiacek.weatherapp.ui.weather
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.wiacek.weatherapp.R
+import com.wiacek.weatherapp.databinding.ActivityWeatherBinding
 
 /**
  * Created by wiacek.dawid@gmail.com
@@ -12,6 +14,6 @@ class WeatherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var binding = DataBindingUtil.setContentView<ActivityWeatherBinding>(this, R.layout.activity_weather)
     }
 }
--- app/src/main/java/com/wiacek/weatherapp/ui/weather/WeatherViewModel.kt
+++ app/src/main/java/com/wiacek/weatherapp/ui/weather/WeatherViewModel.kt
@@ -1,10 +1,43 @@
 package com.wiacek.weatherapp.ui.weather
 
 import android.databinding.BaseObservable
+import android.databinding.Bindable
+import com.wiacek.weatherapp.BR
 
 /**
  * Created by wiacek.dawid@gmail.com
  */
-class WeatherViewModel : BaseObservable() {
+class WeatherViewModel(currentCondition: String = "",
+                       temperature: String = "",
+                       windSpeed: String = "",
+                       windDirection: String = "") : BaseObservable() {
+
+    @get:Bindable
+    var currentCondition = currentCondition
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.currentCondition)
+        }
+
+    @get:Bindable
+    var temperature = temperature
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.temperature)
+        }
+
+    @get:Bindable
+    var windSpeed = windSpeed
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.windSpeed)
+        }
+
+    @get:Bindable
+    var windDirection = windDirection
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.windDirection)
+        }
 
 }
